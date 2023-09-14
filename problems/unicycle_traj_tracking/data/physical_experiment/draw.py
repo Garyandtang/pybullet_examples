6d8b7cf8b505cc0c8bab80012f6069fc8a1c5043
Garyandtang <30773670+Garyandtang@users.noreply.github.com>
2023-09-14 10:05:38 +0800

    draw

---
+++ problems/unicycle_traj_tracking/data/physical_experiment/draw.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from problems.unicycle_traj_tracking.data.butterfly_tracking.draw import load_data_and_save_figure
+
+def main():
+    gmpc_solver_time = np.load('Geomtric_model_predictive_control_store_solve_time.npy')
+    nmpc_solver_time = np.load('Nonlinear_model_predictive_control_store_solve_time.npy')
+    plt.figure()
+    plt.boxplot([gmpc_solver_time, nmpc_solver_time], labels=['GMPC', 'NMPC'],showfliers=False, patch_artist=True )
+    plt.ylabel('Solve time (s)')
+    plt.tight_layout()
+    plt.savefig('solve_time.eps')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
